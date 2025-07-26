566c7791e45f64d3656b8a692b8beb38ac0129ab
Om Sapate <omsapate@outlook.com>
2025-07-26 17:00:57 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
-const hour =19;
+anime({
+  targets: '#sunflower',
+  scale: [
+    { value: 1, duration: 800 },
+    { value: 1.05, duration: 800 }
+  ],
+  easing: 'easeInOutSine',
+  direction: 'alternate',
+  loop: true
+});
+
+new Date().getHours();
 if (hour < 6 || hour >= 18) {
   document.body.style.background = "linear-gradient(to top, #0b0c2a, #1e1e40)";
 } else {
