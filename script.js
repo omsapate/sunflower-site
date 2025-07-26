79fe977c95774b819c4f8aadf4153817ba59733b
Om Sapate <omsapate@outlook.com>
2025-07-26 16:23:09 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+const hour =19;
+if (hour < 6 || hour >= 18) {
+  document.body.style.background = "linear-gradient(to top, #0b0c2a, #1e1e40)";
+} else {
+  document.body.style.background = "linear-gradient(to top, #c2f0ff, #87ceeb)";
+}
